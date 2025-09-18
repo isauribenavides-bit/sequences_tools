979aebad0ebf69497c7eac99723fa9b2a4622bab
isauri <isauribenavides@gmail.com>
2025-09-18 14:21:07 -0600

    agregue un nuevo archivo

---
+++ sequences_tools.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Este programa sirve para convertir secuencias
+
+# El usuario puede ingresar una secuencia de DNA
+dna_seq=$1
+
+# Convertir secuencias a mayusculas
+dna_seq=$(echo "$dna_seq" | tr '[:lower:]' '[:upper:]')
+
+
+# DNA A RNA
+rna_seq=$(echo "$dna_seq" | tr 'T' 'U')
+
+# Complementaria
+comp_seq=$(echo "$dna_seq" | tr 'ATCG' 'TAGC')
+
+# Complementaria inversa
+rev_comp=$(echo "$comp_seq" | rev)
+
+echo "DNA:                 $dna_seq"
+echo "mRNA:                $rna_seq"
+echo "Complementaria:      $comp_seq"
+echo "Comp. inversa:       $rev_comp"
+
